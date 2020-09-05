72aae3f5563c37b1eab7884832998daf2e8d3209
Anubhav8484 <70816943+Anubhav8484@users.noreply.github.com>
2020-09-05 23:27:42 +0530

    Add files via upload

---
+++ future.go
@@ -0,0 +1,88 @@
+type Future struct {
+	Cancelled         bool
+	Done              bool
+	Running           bool
+	Result            interface{}
+	InterfaceChannel <-chan interface{}
+}
+
+
+func (f *Future) cancel() bool{
+      if f.Running{
+	     return false
+	  f.Cancelled= true;
+	  return true;
+}
+
+
+func (f *Future) cancelled() bool{
+      if f.Running{
+	     return false
+	  return f.Cancelled
+}
+
+func (f *Future) running() bool{
+      return f.Running
+}
+
+func (f *Future) done() bool{
+      return (f.Cancelled | f.Done)
+	      
+}
+
+//result function when timeout is not given
+func (f *Future) result() interface{} {
+	if f.Done {
+		return f.result
+	}
+	f.Result = <-f.InterfaceChannel
+	return f.Result
+}
+
+//result function when timeout is  given
+func (f *Future,timeout time.Duration) result interface{} {
+	if f.Done {
+		return f.Result
+	}
+	timeoutChannel := time.After(timeout)
+	select {
+	case res := <-f.InterfaceChannel:
+		f.Result = res
+		f.Done = true
+	case <-timeoutChannel:
+		f.Result = nil
+		f.Done = true
+	}
+	return f.Result
+}
+
+//submit function
+func ThreadPoolExecutorSubmit(implem interface{}, args ...interface{}) *Future {
+    var f Future
+	
+    f.Running = false
+	f.Cancelled = false
+	f.Done = false
+	f.Result =nil
+	
+	valIn := make([]reflect.Value, len(args), len(args))    
+
+	func_name := reflect.ValueOf(implem)         //function name to be called
+    
+	for idx, elt := range args {
+		valIn[idx] = reflect.ValueOf(elt)        //function arguments
+	}
+	
+	
+
+	go func() {
+	    f.Running = true
+		res := func_name.Call(valIn)
+		for idx, elt := range res {
+			f.InterfaceChannel <- res[idx].Interface()
+		f.Running = false
+		f.Done = true
+	}()
+
+	return &f
+}
